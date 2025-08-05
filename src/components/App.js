37a6b92f8cb94c56ed65d4ed032d00c2e8231d80
Akash Das <akashakki1100@gmail.com>
2025-08-05 14:06:25 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,19 @@
 
 import React from "react";
 import './../styles/App.css';
+import PageLayout from "./PageLayout";
 
 const App = () => {
-  const relatives = [
-    { key: "relativeListItem1", name: "Uncle Raj" },
-    { key: "relativeListItem2", name: "Aunt Sita" },
-    { key: "relativeListItem3", name: "Cousin Neha" },
-    { key: "relativeListItem4", name: "Grandpa Mohan" },
-    { key: "relativeListItem5", name: "Grandma Laxmi" }
-  ]
+ 
   return (
-    <div>
-        {/* Do not remove the main div */}
-       <ol key='relativeList'>
-       {
-        relatives.map((relatives) =>  (
-          <li key={relatives.key}>{relatives.name}</li>
-        ))
-       }
-       </ol>
-    </div>
+    <PageLayout
+    header={<h1>Welcome to my website</h1>}
+    footer={<p>© 2023 My Website. All rights reserved.</p>}
+
+    >
+    <p>This is the content of my website.</p>
+
+    </PageLayout>
   )
 }
 
